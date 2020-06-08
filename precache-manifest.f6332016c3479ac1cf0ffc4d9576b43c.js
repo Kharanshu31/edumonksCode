a1ba6f684099b9af282cd1b3dd3ef461d8403b81
Kharanshu <dewankharanshu@gmail.com>
2020-06-09 03:04:14 +0530

    Updates

---
+++ precache-manifest.f6332016c3479ac1cf0ffc4d9576b43c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fd7163ad034ebc5921af2b7881b46a2",
+    "url": "/edumonk/index.html"
+  },
+  {
+    "revision": "6e6428ef3f43f3d27b1d",
+    "url": "/edumonk/static/css/main.5ecc05f2.chunk.css"
+  },
+  {
+    "revision": "a5f74efbc44f42445fae",
+    "url": "/edumonk/static/js/2.ef81d55a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/edumonk/static/js/2.ef81d55a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e6428ef3f43f3d27b1d",
+    "url": "/edumonk/static/js/main.73b3566a.chunk.js"
+  },
+  {
+    "revision": "71b2549324df76a6ae03",
+    "url": "/edumonk/static/js/runtime-main.30080eaf.js"
+  },
+  {
+    "revision": "fd8fbaff860fbb8c5d0b23ededa283d5",
+    "url": "/edumonk/static/media/main.fd8fbaff.jpg"
+  },
+  {
+    "revision": "67fad8a7a6f33671bf95581421902875",
+    "url": "/edumonk/static/media/study.67fad8a7.jpg"
+  }
+]);
